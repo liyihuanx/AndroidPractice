bd9c61933b9200e0a153a68219186af3b75afc55
liyihuan <liyihuan@kding.com>
2021-03-12 15:29:37 +0800

    0312

--- app/src/main/java/liyihuan/app/android/androidpractice/touchevent/BadRecyclerView.kt
+++ app/src/main/java/liyihuan/app/android/androidpractice/touchevent/BadRecyclerView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.MotionEvent
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.abs
 
 /**
  * @ClassName: BadRecyclerView
@@ -13,11 +14,41 @@ import androidx.recyclerview.widget.RecyclerView
  */
 
 class BadRecyclerView : RecyclerView {
+    private var lastX = -1
+    private var lastY = -1
+
     constructor(context: Context) : this(context, null)
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
-    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
-        return super.dispatchTouchEvent(ev)
-    }
+    /**
+     * 内部拦截
+     */
+//    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+//        var touchX = ev.x.toInt()  // 记录touch坐标
+//        var touchY = ev.y.toInt()
+//        when (ev.action) {
+//            MotionEvent.ACTION_DOWN -> { // 父亲 --> return false,遍历子View询问是否有人处理？？
+//                // 请求是否允许拦截事件
+//                parent.requestDisallowInterceptTouchEvent(true) // 告诉父View不允许拦截
+//            }
+//
+//            MotionEvent.ACTION_MOVE -> {
+//                // 用现在的坐标 - 上一次的坐标
+//                if (abs(ev.x - lastX) > abs(ev.y - lastY)) {
+//                    parent.requestDisallowInterceptTouchEvent(false)
+//                    return false
+//                }
+//            }
+//
+//            MotionEvent.ACTION_UP -> {
+//
+//            }
+//        }
+//
+//        lastX = touchX // 保存上一次的坐标
+//        lastY = touchY
+//
+//        return super.dispatchTouchEvent(ev) // 自己处理
+//    }
 }
--- app/src/main/java/liyihuan/app/android/androidpractice/touchevent/BadViewPager.kt
+++ app/src/main/java/liyihuan/app/android/androidpractice/touchevent/BadViewPager.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.MotionEvent
 import androidx.viewpager.widget.ViewPager
+import kotlin.math.abs
 
 /**
  * @ClassName: BadViewPager
@@ -16,11 +17,40 @@ class BadViewPager : ViewPager {
     constructor(context: Context) : this(context, null)
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
 
+    /**
+     * 内部拦截
+     */
+//    override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
+//        if (ev.action == MotionEvent.ACTION_DOWN) {
+//            super.onInterceptTouchEvent(ev)
+//            return false
+//        }
+//        return true
+//    }
 
-    override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
-        return false
+
+    /**
+     * 外部拦截
+     */
+    private var lastX = -1
+    private var lastY = -1
+    override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
+        var touchX = ev.x.toInt()  // 记录touch坐标
+        var touchY = ev.y.toInt()
+        when (ev.action) {
+            MotionEvent.ACTION_DOWN -> {
+                lastX = touchX
+                lastY = touchY
+            }
+            MotionEvent.ACTION_MOVE -> {
+                // 用现在的坐标 - 上一次的坐标
+                if (abs(ev.x - lastX) > abs(ev.y - lastY)) {
+                    return true
+
+                }
+            }
+        }
+        return super.onInterceptTouchEvent(ev)
+
     }
-
-
-
 }
