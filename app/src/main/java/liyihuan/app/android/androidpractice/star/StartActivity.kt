5d18879e03432ea723e3e9aa5b56919b4c7c826b
liyihuan <liyihuan@kding.com>
2021-03-17 16:38:51 +0800

    0317

---
+++ app/src/main/java/liyihuan/app/android/androidpractice/star/StartActivity.kt
@@ -0,0 +1,18 @@
+package liyihuan.app.android.androidpractice.star
+
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
+import android.os.Bundle
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.Animation
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_start.*
+import liyihuan.app.android.androidpractice.R
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
+    }
+}
